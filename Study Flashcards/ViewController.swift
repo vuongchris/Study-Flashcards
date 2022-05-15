4e81ebb5493fa6d703eedf5dd61fc8b07b7ad452
briana00 <briana.r.margetts@student.uts.edu.au>
2022-05-15 23:46:13 +1000

    added search function (doesn't search yet)

--- Study Flashcards/ViewController.swift
+++ Study Flashcards/ViewController.swift
@@ -7,11 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Results: UIViewController{
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
 
+class ViewController: UIViewController, UISearchResultsUpdating {
+    
+    @IBOutlet var table: UITableView!
+
+    let searchController = UISearchController(searchResultsController: Results())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // search title and search bar
+        title = "Search Subject"
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+        
+    }
+    // updates search after every character
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else {
+            return
+        }
+        let rs = searchController.searchResultsController as? Results
+        rs?.view.backgroundColor = .systemRed
+        print(text)
     }
 
 
