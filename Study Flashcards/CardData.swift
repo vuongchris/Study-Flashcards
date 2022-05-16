0c6989d6f25d0db34e6fcf4433c60b9de7418df1
briana00 <briana.r.margetts@student.uts.edu.au>
2022-05-16 14:29:37 +1000

    populating more subjects

--- Study Flashcards/CardData.swift
+++ Study Flashcards/CardData.swift
@@ -9,11 +9,30 @@ import Foundation
 
 // some sample cards
 let CardData: [Card] = [
+    //sample biology cards
     Card(subject: "Biology", question: "What is Prophase I", answer: "Each chromosome comes near its replicated chromosome pair. Nucleus dissolves at this stage. Crossing Over (exchange of genetic material) occurs at this phase."),
     
     Card(subject: "Biology", question: "What is Metaphase I", answer: "The pairs of homologous chromosomes line up in the center of the cell. The centromere of each chromatid pair attaches to one spindle fibre."),
     
     Card(subject: "Biology", question: "What is Anaphase I", answer: "Homologous chromosomes are pulled to opposite ends of the cell. Note that chromatids do not separate - each duplicated chromosome still has two chromatids."),
     
-    Card(subject: "Biology", question: "What is Telophase I", answer: "The cytoplasm divides and two new cells form. Each new cell has one duplicated chromosome from each similar pair.")
+    Card(subject: "Biology", question: "What is Telophase I", answer: "The cytoplasm divides and two new cells form. Each new cell has one duplicated chromosome from each similar pair."),
+    //sample maths cards
+    Card(subject: "Maths", question:"What is", answer:""),
+    
+    Card(subject: "Maths", question:"What is, answer:""),
+    
+    Card(subject: "Maths", question:"What is", answer:""),
+    
+    Card(subject: "Maths", question:"What is", answer:""),
+    
+    //sample history cards
+    
+    Card(subject: "History", question:"", answer:""),
+    
+    Card(subject: "History", question:"", answer:""),
+    
+    Card(subject: "History", question:"", answer:""),
+    
+    Card(subject: "History", question:"", answer:"")
 ]
